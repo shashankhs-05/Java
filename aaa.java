1948a89d717fac3a65a78676720c7d9dff2c7ce7
Shashank.H.S <111795983+shashankhs-05@users.noreply.github.com>
2022-12-15 15:08:30 +0530

    Update aaa.java

--- aaa.java
+++ aaa.java
@@ -3,7 +3,7 @@
   int accountNumber;
   String accountType;
   double balance;
-
+	double service_charge;
    Account(String customerName, int accountNumber, String accountType, double balance) {
     this.customerName = customerName;
     this.accountNumber = accountNumber;
@@ -33,11 +33,26 @@
  void displayBalance() {
     System.out.println("The current balance is: " + balance);
   }
-}
+ void withdrawl(double amount) {
+	if(accountType=="Current"){
+if(balance<50){
+service_charge+=10;
+System.out.println("Withdrawl not allowed.Balance below minimum");
+
+System.out.println("The current service charge is: " + service_charge);
+}else
+ balance -= amount;
+  }
+}}
 
 class aaa{
 public static void main(String args[]){
-Account shashank=new Account("Shashank",12345,"Savings",45);
+Account shashank=new Account("Shashank",12345,"Current",45);
 System.out.println("Account holder: " + shashank.getCustomerName());
 System.out.println("Account type: " + shashank.getAccountType());
+shashank.deposit(45);
+shashank. displayBalance();
+shashank.withdrawl(78);
+shashank.withdrawl(8);
+shashank. displayBalance();
 }}
